c4cd6a54f79c0d6255c53e9810450129cb974d70
bxb5733 <bxb5733@g.rit.edu>
2018-03-17 16:14:52 -0400

    Inventory files

---
+++ Escape Room/Assets/Scripts/Inventory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    //Populate early
+    public List<Item> database = new List<Item>();
+
+    //Gets populated in-game
+    public List<Item> currentInventory = new List<Item>();
+
+    public Item heldItem;
+    private bool inventoryOpen = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        heldItem = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Inventory button press
+        if (Input.GetKey("I"))
+        {
+            //Open or close inventory
+            inventoryOpen = !inventoryOpen;
+            Debug.Log("Inventory Button pressed");
+        }
+    }
+
+    void Additem(int itemID)
+    {
+
+    }
+
+    void RemoveItem(int itemID)
+    {
+
+    }
+
+    void SelectItem(int itemID)
+    {
+
+    }
+}
---
+++ Escape Room/Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public Texture2D itemIcon;
+    public int itemID;
+    public string itemName;
+    public string itemDesc;
+}
