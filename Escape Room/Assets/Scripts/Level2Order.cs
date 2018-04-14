00e539b1e465e9c67dddc74c7134d942720f5feb
bxb5733 <bxb5733@g.rit.edu>
2018-04-14 16:54:59 -0400

    Some scene changes

---
+++ Escape Room/Assets/Scripts/Level2Order.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;       //Allows us to use Lists. 
+
+public class Level2Order : MonoBehaviour
+{
+
+    public static Level2Order instance = null;              //Current level number, expressed in game as "Day 1".
+
+    //bools to check if item is interactale
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+
+    }
+
+
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
