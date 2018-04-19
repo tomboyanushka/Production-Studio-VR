abe56cd1d9fb01378323d4e09017195884745f56
bxb5733 <bxb5733@g.rit.edu>
2018-04-19 01:17:51 -0400

    Minor level changes. Need finalizing

---
+++ Escape Room/Assets/Scripts/Pillow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Colliding");
+        if (col.CompareTag("Tooth"))
+        {
+            Debug.Log("Colliding");
+            //Animation
+            //Create object of coin too
+            Destroy(col.gameObject);
+        }
+    }
+}
