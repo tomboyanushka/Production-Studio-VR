9ba4a7c4665c7b606331e5631ea4289c6ea4d41c
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-04-21 00:13:18 -0400

    Working on The level 1 floor scene - Do not change anything

---
+++ Escape Room/Assets/Scripts/Selectable.cs
@@ -0,0 +1,47 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour, IInputClickHandler
+{
+    [Tooltip("Object color changes to this when selected.")]
+    public Color SelectedColor = Color.red;
+
+    private Color originalColor;
+    private Material cachedMaterial;
+
+    private void OnEnable()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        if (InputManager.Instance != null)
+        {
+            InputManager.Instance.RemoveGlobalListener(gameObject);
+        }
+    }
+
+    public void OnSelectObject()
+    {
+        //cachedMaterial.SetColor("_Color", SelectedColor);
+    }
+
+    public void OnClearSelection()
+    {
+        //cachedMaterial.SetColor("_Color", originalColor);
+    }
+
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        OnClearSelection();
+        if (eventData.selectedObject == gameObject)
+        {
+            OnSelectObject();
+        }
+    }
+
+}
