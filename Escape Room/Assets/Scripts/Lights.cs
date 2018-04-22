d448034f5c67f018d3437099abe810ff354a7245
bxb5733 <burhanuddin.mb@gmail.com>
2018-04-21 22:17:57 -0400

    item interactions in level 2 done

---
+++ Escape Room/Assets/Scripts/Lights.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public bool areLightsOn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        areLightsOn = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!areLightsOn)
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            } 
+        else
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+}
