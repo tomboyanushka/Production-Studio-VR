d44e9edf3fe3857a699526d376192812314855b7
bxb5733 <burhanuddin.mb@gmail.com>
2018-04-25 23:48:30 -0400

    Adding audio manager. Things arent working as I thought they would

---
+++ Escape Room/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField] private AudioClip wallBreakSound;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void WallBreak()
+    {
+        source.PlayOneShot(wallBreakSound);
+    }
+}
--- Escape Room/Assets/Scripts/WallLvl2.cs
+++ Escape Room/Assets/Scripts/WallLvl2.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class WallLvl2 : MonoBehaviour
 {
     [SerializeField] GameObject tooth;
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+
+    // Use this for initialization
+    void Start () {
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,6 +25,7 @@ public class WallLvl2 : MonoBehaviour
             // Play music of wall shattering and tooth breaking
             Instantiate(tooth, new Vector3(5.0f, 1.0f, 0.0f), Quaternion.identity);
             GameObject.Find("pillow").GetComponent<Pillow>().isWallBroken = true;
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioManager>().WallBreak();
             Destroy(gameObject);
         }
     }
